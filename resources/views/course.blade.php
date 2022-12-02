745412905b5d9135b2a758cddf69746621868d13
wellyct <wellyct@yahoo.com>
2022-12-02 16:03:44 +0700

    add pics

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -5,116 +5,137 @@
 <h1>Course</h1>
 
 <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-    <h1 class="display-4 fw-normal">Daftar Kursus</h1>
-    <p class="fs-5 text-muted">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization.</p>
-  </div>
-<div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+  <h1 class="display-4 fw-normal">Daftar Paket Kursus Mengemudi</h1>
+  <p class="fs-5 text-muted">Kami menawarkan Paket Latihan Manual dan Matic yang tentu saja dengan harga bersaing dan pelayanan yang maksimal.
+    Kita akan diajarkan mulai dari dasar seperti pengenalan bagian-bagian mobil, pedal gas, kopling, rem, tanjakan atau turunan, dan juga parkir.</p>
+</div>
+
+<div><h4>Paket Mobil Manual</h4></div>
+  <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
     <div class="col">
       <div class="card mb-4 rounded-3 shadow-sm">
         <div class="card-header py-3">
-          <h4 class="my-0 fw-normal">Free</h4>
+          <h4 class="my-0 fw-normal">Suka-suka</h4>
         </div>
         <div class="card-body">
-          <h1 class="card-title pricing-card-title">$0<small class="text-muted fw-light">/mo</small></h1>
+          <h1 class="card-title pricing-card-title">IDR 70<small class="text-muted fw-light">  ribu</small></h1>
           <ul class="list-unstyled mt-3 mb-4">
-            <li>10 users included</li>
-            <li>2 GB of storage</li>
-            <li>Email support</li>
-            <li>Help center access</li>
+            <li>Biaya per jam</li>
+            <li>Cash sesuai pengambilan</li>
+            <li>2 – 12 x Pertemuan</li>
+            <li style="text-decoration: line-through;">Administrasi 20rb</li>
+            <li style="text-decoration: line-through;">Antar jemput</li>
           </ul>
-          <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
         </div>
       </div>
     </div>
     <div class="col">
       <div class="card mb-4 rounded-3 shadow-sm">
         <div class="card-header py-3">
-          <h4 class="my-0 fw-normal">Pro</h4>
+          <h4 class="my-0 fw-normal">Private</h4>
         </div>
         <div class="card-body">
-          <h1 class="card-title pricing-card-title">$15<small class="text-muted fw-light">/mo</small></h1>
+          <h1 class="card-title pricing-card-title">IDR 480<small class="text-muted fw-light"> ribu</small></h1>
           <ul class="list-unstyled mt-3 mb-4">
-            <li>20 users included</li>
-            <li>10 GB of storage</li>
-            <li>Priority email support</li>
-            <li>Help center access</li>
+            <li>Administrasi 20rb</li>
+            <li>10 x Pertemuan</li>
+            <li>max 1 jam / ±20km</li>
+            <li style="text-decoration: line-through;">Antar-Jemput</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
           </ul>
-          <button type="button" class="w-100 btn btn-lg btn-primary">Get started</button>
+          
         </div>
       </div>
     </div>
     <div class="col">
       <div class="card mb-4 rounded-3 shadow-sm border-primary">
         <div class="card-header py-3 text-bg-primary border-primary">
-          <h4 class="my-0 fw-normal">Enterprise</h4>
+          <h4 class="my-0 fw-normal">VIP</h4>
         </div>
         <div class="card-body">
-          <h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/mo</small></h1>
+          <h1 class="card-title pricing-card-title">IDR 630<small class="text-muted fw-light"> ribu</small></h1>
           <ul class="list-unstyled mt-3 mb-4">
-            <li>30 users included</li>
-            <li>15 GB of storage</li>
-            <li>Phone and email support</li>
-            <li>Help center access</li>
+            <li>Administrasi 20rb</li>
+            <li>12 x Pertemuan</li>
+            <li>max 2 jam / ±40km</li>
+            <li>Antar-Jemput (max 10 Km)</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
           </ul>
-          <button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
+
         </div>
       </div>
     </div>
   </div>
 
-  <h2 class="display-6 text-center mb-4">Compare plans</h2>
+  <div><h4>Paket Mobil Automatic</h4></div>
+  <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+    <div class="col">
+      <div class="card mb-4 rounded-3 shadow-sm">
+        <div class="card-header py-3">
+          <h4 class="my-0 fw-normal">Suka-suka</h4>
+        </div>
+        <div class="card-body">
+          <h1 class="card-title pricing-card-title">IDR 95<small class="text-muted fw-light">  ribu</small></h1>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>Biaya per jam</li>
+            <li>Cash sesuai pengambilan</li>
+            <li>2 – 12 x Pertemuan</li>
+            <li style="text-decoration: line-through;">Administrasi 20rb</li>
+            <li style="text-decoration: line-through;">Antar jemput</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card mb-4 rounded-3 shadow-sm">
+        <div class="card-header py-3">
+          <h4 class="my-0 fw-normal">Private</h4>
+        </div>
+        <div class="card-body">
+          <h1 class="card-title pricing-card-title">IDR 730<small class="text-muted fw-light"> ribu</small></h1>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>Administrasi 20rb</li>
+            <li>10 x Pertemuan</li>
+            <li>max 1 jam / ±20km</li>
+            <li style="text-decoration: line-through;">Antar-Jemput</li>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+          </ul>
+          
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card mb-4 rounded-3 shadow-sm border-primary">
+        <div class="card-header py-3 text-bg-primary border-primary">
+          <h4 class="my-0 fw-normal">VIP</h4>
+        </div>
+        <div class="card-body">
+          <h1 class="card-title pricing-card-title">IDR 830<small class="text-muted fw-light"> ribu</small></h1>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>Administrasi 20rb</li>
+            <li>12 x Pertemuan</li>
+            <li>max 2 jam / ±40km</li>
+            <li>Antar-Jemput (max 10 Km)</li>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+            <li> </li>
+          </ul>
 
-  <div class="table-responsive">
-    <table class="table text-center">
-      <thead>
-        <tr>
-          <th style="width: 34%;"></th>
-          <th style="width: 22%;">Free</th>
-          <th style="width: 22%;">Pro</th>
-          <th style="width: 22%;">Enterprise</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row" class="text-start">Public</th>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"/></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"/></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"/></svg></td>
-        </tr>
-        <tr>
-          <th scope="row" class="text-start">Private</th>
-          <td></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"/></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"/></svg></td>
-        </tr>
-      </tbody>
-
-      <tbody>
-        <tr>
-          <th scope="row" class="text-start">Permissions</th>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"/></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"/></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"/></svg></td>
-        </tr>
-        <tr>
-          <th scope="row" class="text-start">Sharing</th>
-          <td></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"/></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"/></svg></td>
-        </tr>
-        <tr>
-          <th scope="row" class="text-start">Unlimited members</th>
-          <td></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"/></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"/></svg></td>
-        </tr>
-        <tr>
-          <th scope="row" class="text-start">Extra security</th>
-          <td></td>
-          <td></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"/></svg></td>
-        </tr>
-      </tbody>
-    </table>
+        </div>
+      </div>
+    </div>
   </div>
+  
 @endsection
--- resources/views/fleet.blade.php
+++ resources/views/fleet.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.main')
 
 @section('content')
+<style>
+.b-example-divider {
+  height: 3rem;
+  background-color: rgba(0, 0, 0, .1);
+  border: solid rgba(0, 0, 0, .15);
+  border-width: 1px 0;
+  box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+}
+</style>
+
   <h1>Fleet</h1>
   <section class="text-center container">
     <div class="row py-lg-5">
@@ -14,7 +24,7 @@
 
   <div class="album py-5 bg-light">
     <div class="container">
-
+      <h4>Mobil Manual</h4>
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         <div class="col">
           <div class="card shadow-sm">
@@ -50,43 +60,52 @@
             </div>
           </div>
         </div>
-
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-            <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <div class="d-flex justify-content-between align-items-center">
-              </div>
-            </div>
-          </div>
-        </div>
-
-
+      </div>
     </div>
+  </div>
+
+  <div class="b-example-divider"></div>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+      <h4>Mobil Automatic</h4>
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+      <img class="img-thumbnail" src="img/car4.png" alt="" width="100%">
+
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <img class="img-thumbnail" src="img/car5.png" alt="" width="100%">
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <img class="img-thumbnail" src="img/car6.png" alt="" width="100%">
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 @endsection
